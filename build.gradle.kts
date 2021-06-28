5eb6924f0fa4df6d16827f2c95469e555db33d57
Leo40Git <7997354+Leo40Git@users.noreply.github.com>
2021-06-28 13:13:46 +0300

    command advancements!

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     kotlin("jvm") version "1.5.10"
 }
@@ -7,12 +9,23 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+
+    maven {
+        name = "Kotlin Discord"
+        url = uri("https://maven.kotlindiscord.com/repository/maven-public/")
+    }
 }
 
 dependencies {
     implementation(kotlin("stdlib"))
 
-    implementation("dev.kord:kord-core:0.7.0")
+    implementation("com.kotlindiscord.kord.extensions:kord-extensions:1.4.1-SNAPSHOT")
     implementation("io.fusionauth:fusionauth-jwt:4.2.0")
     implementation("org.kohsuke:github-api:1.131")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "16"
+
+    kotlinOptions.freeCompilerArgs += "-Xopt-in=kotlin.RequiresOptIn"
 }
--- src/main/java/io/github/leo40git/smars/Main.kt
+++ src/main/java/io/github/leo40git/smars/Main.kt
@@ -1,12 +1,16 @@
 package io.github.leo40git.smars
 
+import com.kotlindiscord.kord.extensions.ExtensibleBot
+import com.kotlindiscord.kord.extensions.commands.converters.impl.message
+import com.kotlindiscord.kord.extensions.commands.parser.Arguments
+import com.kotlindiscord.kord.extensions.extensions.Extension
+import com.kotlindiscord.kord.extensions.utils.getJumpUrl
 import dev.kord.core.Kord
 import dev.kord.core.behavior.channel.createMessage
 import dev.kord.core.behavior.reply
 import dev.kord.core.entity.Message
 import dev.kord.core.entity.channel.TextChannel
 import dev.kord.core.event.message.MessageCreateEvent
-import dev.kord.core.on
 import dev.kord.rest.builder.message.MessageCreateBuilder
 import io.ktor.client.*
 import io.ktor.client.engine.cio.*
@@ -37,82 +41,136 @@ fun getEnvString(name: String, desc: String): String {
     return value
 }
 
-suspend fun main() {
-    val token = getEnvString("TOKEN", "your Discord bot's token")
-    val appId = getEnvString("GH_APP_ID", "your GitHub App's ID")
-    val keyPath = getEnvString("GH_APP_KEY", "the path to your GitHub App's PEM file")
-    val repo = getEnvString("GH_REPO", "the GitHub repository to upload releases to")
+val token = getEnvString("TOKEN", "your Discord bot's token")
+val appId = getEnvString("GH_APP_ID", "your GitHub App's ID")
+val keyPath = getEnvString("GH_APP_KEY", "the path to your GitHub App's PEM file")
+val repo = getEnvString("GH_REPO", "the GitHub repository to upload releases to")
 
-    val client = HttpClient(CIO) {
-        install(JsonFeature) {
-            serializer = KotlinxSerializer()
-        }
+val client = HttpClient(CIO) {
+    install(JsonFeature) {
+        serializer = KotlinxSerializer()
     }
-    val generator = ReleaseGenerator(client)
-    val uploader = ReleaseUploader(appId, keyPath, repo)
+}
+val generator = ReleaseGenerator(client)
+val uploader = ReleaseUploader(appId, keyPath, repo)
 
-    val kord = Kord(token)
+var bot: ExtensibleBot? = null
 
-    kord.on<MessageCreateEvent> {
-        when (message.channel.id) {
-            Constants.CHANNEL_CONFIG_ID -> {
-                if (message.author?.id != Constants.USER_OWNER_ID)
-                    return@on
-                if (message.content == "!shutdown") {
-                    message.reply(true) { content = "Shutting down..." }
-                    kord.shutdown()
-                }
-            }
-            Constants.CHANNEL_ROLLING_ID -> {
-                if (message.author?.id != Constants.USER_LIBBIE_ID)
-                    return@on
-                for (attach in message.attachments) {
-                    if (!attach.filename.startsWith("Shang_Mu_Architect_")
-                        || !attach.filename.endsWith(".zip"))
-                            continue
-                    val result = generator.generate(attach.url, attach.filename)
-                    if (result.isSuccess) {
-                        val release = result.getOrThrow()
-                        kord.getChannelOf<TextChannel>(Constants.CHANNEL_LOG_ID)?.createMessage {
-                            embed {
-                                color = Colors.YELLOW
-                                title = "**HMM!** New release generated!"
-                                description = "Now uploading v${release.version}..."
-                            }
-                        }
-                        val url = uploader.upload(result.getOrThrow())
-                        kord.getChannelOf<TextChannel>(Constants.CHANNEL_LOG_ID)?.createMessage {
-                            embed {
-                                color = Colors.GREEN
-                                title = "**YAY!** New release uploaded!"
-                                description = "Successfully uploaded v${release.version}! Look at it [here]($url)!"
-                            }
-                        }
-                    } else {
-                        kord.getChannelOf<TextChannel>(Constants.CHANNEL_LOG_ID)?.createMessage {
-                            embed {
-                                color = Colors.RED
-                                title = "**ACK!** Failed to generate new release!"
-                                field {
-                                    name = "Reason"
-                                    value = result.exceptionOrNull()?.message ?: "Unknown"
-                                }
-                                field {
-                                    name = "Stack trace"
-                                    value = result.exceptionOrNull()?.stackTraceToCodeBlock() ?: "<null>"
-                                }
-                            }
-                        }
+suspend fun onMessage(message: Message) {
+    if (message.author?.id != Constants.USER_LIBBIE_ID)
+        return
+    for (attach in message.attachments) {
+        if (!attach.filename.startsWith("Shang_Mu_Architect_")
+            || !attach.filename.endsWith(".zip"))
+            continue
+        val result = generator.generate(attach.url, attach.filename)
+        if (result.isSuccess) {
+            val release = result.getOrThrow()
+            bot!!.getKoin().get<Kord>().getChannelOf<TextChannel>(Constants.CHANNEL_LOG_ID)?.createMessage {
+                embed {
+                    color = Colors.YELLOW
+                    title = "**HMM!** New release generated!"
+                    description = "Now uploading v${release.version}..."
+                    footer {
+                        text = "Message:" + message.getJumpUrl()
                     }
-
-                    break
                 }
             }
-            else -> return@on
+            val url = uploader.upload(result.getOrThrow())
+            bot!!.getKoin().get<Kord>().getChannelOf<TextChannel>(Constants.CHANNEL_LOG_ID)?.createMessage {
+                embed {
+                    color = Colors.GREEN
+                    title = "**YAY!** New release uploaded!"
+                    description = "Successfully uploaded v${release.version}! Look at it [here]($url)!"
+                    footer {
+                        text = "Message:" + message.getJumpUrl()
+                    }
+                }
+            }
+        } else {
+            bot!!.getKoin().get<Kord>().getChannelOf<TextChannel>(Constants.CHANNEL_LOG_ID)?.createMessage {
+                embed {
+                    color = Colors.RED
+                    title = "**ACK!** Failed to generate new release!"
+                    field {
+                        name = "Reason"
+                        value = result.exceptionOrNull()?.message ?: "Unknown"
+                        inline = true
+                    }
+                    field {
+                        name = "Stack trace"
+                        value = result.exceptionOrNull()?.stackTraceToCodeBlock() ?: "<null>"
+                    }
+                    footer {
+                        text = "Message:" + message.getJumpUrl()
+                    }
+                }
+            }
+        }
+
+        break
+    }
+}
+
+class ScraperExtension : Extension() {
+    override val name: String = "scraper"
+
+    private fun configCheck(event: MessageCreateEvent): Boolean {
+        return (event.message.channelId == Constants.CHANNEL_CONFIG_ID
+                && event.message.author?.id == Constants.USER_OWNER_ID)
+    }
+
+    override suspend fun setup() {
+        command(::ScrapeArgs) {
+            name = "scrape"
+            description = "Scrapes a rolling release from a specific message."
+
+            check { event -> configCheck(event) }
+
+            action {
+                with(arguments) {
+                    onMessage(target)
+                }
+            }
+        }
+
+        command {
+            name = "shutdown"
+            description = "Gracefully shuts down the bot."
+
+            check { event -> configCheck(event) }
+
+            action {
+                message.reply(true) {
+                    content = "**Shutting down...**"
+                }
+                bot.getKoin().get<Kord>().logout()
+            }
         }
     }
 
-    kord.login()
+    class ScrapeArgs : Arguments() {
+        val target by message("target", description = "Message to scrape from",
+            requireGuild = true, requiredGuild = suspend { Constants.CHANNEL_ROLLING_ID })
+    }
+}
+
+suspend fun main() {
+    bot = ExtensibleBot(token) {
+        messageCommands {
+            defaultPrefix = "!"
+        }
+        extensions {
+            add(::ScraperExtension)
+        }
+    }
+
+    bot!!.on<MessageCreateEvent> {
+        if (message.channelId == Constants.CHANNEL_ROLLING_ID && message.author?.id == Constants.USER_LIBBIE_ID)
+            onMessage(message)
+    }
+
+    bot!!.start()
 
     client.close()
 }
