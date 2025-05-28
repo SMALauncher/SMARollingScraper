25dc74bddf3dc592c338029e8a79be89e47ba91a
Leo40Git <7997354+Leo40Git@users.noreply.github.com>
2025-05-28 19:20:36 +0300

    Sentry goin' up

--- src/main/kotlin/io/github/leo40git/smars/App.kt
+++ src/main/kotlin/io/github/leo40git/smars/App.kt
@@ -5,11 +5,15 @@ package io.github.leo40git.smars
 import dev.kord.common.entity.Snowflake
 import dev.kordex.core.ExtensibleBot
 import dev.kordex.core.utils.env
+import dev.kordex.core.utils.envOrNull
 import io.github.leo40git.smars.extensions.ScraperExtension
+import io.github.leo40git.smars.extensions.SentryTestExtension
 import io.github.leo40git.smars.extensions.ShutdownExtension
 import java.io.File
 
 private val TOKEN = env("TOKEN")
+
+private val SENTRY_DSN = envOrNull("SENTRY_DSN")
 
 val GITHUB_APP_ID = env("GITHUB_APP_ID")
 
@@ -26,12 +30,19 @@ val LOG_CHANNEL_ID = Snowflake(857240987214544938)
 suspend fun main() {
 	val bot = ExtensibleBot(TOKEN) {
 		extensions {
+			sentry {
+				enableIfDSN(SENTRY_DSN)
+			}
+
 			add(::ScraperExtension)
 		}
 
 		if (devMode) {
 			extensions {
 				add(::ShutdownExtension)
+
+				if (SENTRY_DSN != null)
+					add(::SentryTestExtension)
 			}
 
 			// In development mode, load any plugins from `src/main/dist/plugin` if it exists.
