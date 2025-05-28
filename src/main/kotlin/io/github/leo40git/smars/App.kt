b2fbb1d749bef17dabc3f2eae0296e60bbc32426
Leo40Git <7997354+Leo40Git@users.noreply.github.com>
2025-05-28 18:58:25 +0300

    constants/misc files nonsense

--- src/main/kotlin/io/github/leo40git/smars/App.kt
+++ src/main/kotlin/io/github/leo40git/smars/App.kt
@@ -1,19 +1,45 @@
+@file:Suppress("MagicNumber")
+
 package io.github.leo40git.smars
 
+import dev.kord.common.entity.Snowflake
 import dev.kordex.core.ExtensibleBot
 import dev.kordex.core.utils.env
 import io.github.leo40git.smars.extensions.ScraperExtension
 import io.github.leo40git.smars.extensions.ShutdownExtension
+import java.io.File
 
 private val TOKEN = env("TOKEN")
+
+val GITHUB_APP_ID = env("GITHUB_APP_ID")
+
+val GITHUB_APP_KEY = env("GITHUB_APP_KEY")
+
+val GITHUB_REPO = env("GITHUB_REPO")
+
+val TEST_SERVER_ID = Snowflake(847428895641042944)
+
+val SCRAPE_CHANNEL_ID = Snowflake(836080736939409418)
+
+val LOG_CHANNEL_ID = Snowflake(857240987214544938)
 
 suspend fun main() {
 	val bot = ExtensibleBot(TOKEN) {
 		extensions {
 			add(::ScraperExtension)
+		}
 
-			if (this@ExtensibleBot.devMode)
+		if (devMode) {
+			extensions {
 				add(::ShutdownExtension)
+			}
+
+			// In development mode, load any plugins from `src/main/dist/plugin` if it exists.
+			plugins {
+				if (File("src/main/dist/plugins").isDirectory) {
+					pluginPath("src/main/dist/plugins")
+				}
+			}
 		}
 	}
 
